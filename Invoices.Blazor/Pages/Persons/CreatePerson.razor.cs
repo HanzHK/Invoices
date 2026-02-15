d264441476c02e8cf684b1561a82f82b32b22131
HanzHKR <kubicek.jan@hotmail.com>
2026-02-15 05:49:48 +0100

    - CreatePerson  page update, language support, snackbar - PersonForm: add input validation

---
+++ Invoices.Blazor/Pages/Persons/CreatePerson.razor.cs
@@ -0,0 +1,27 @@
+﻿using Invoices.Blazor.Pages.Infrastructure.Localization;
+using Invoices.Blazor.Services;
+using Invoices.Shared.Models.Person;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Invoices.Blazor.Pages.Persons
+{
+    public partial class CreatePerson : LocalizationPageBase
+    {
+        private PersonDto person = new();
+        [Inject] public PersonService PersonService { get; set; } = default!;
+        private async Task CreatePersonAsync(PersonDto model)
+        {
+
+
+            var created = await PersonService.CreateAsync(model);
+
+            if (created != null)
+            {
+                Snackbar.Add(T("PersonAdded"), Severity.Success);
+                Nav.NavigateTo("/subjects/list");
+                return;
+            }
+        }
+    }
+}
