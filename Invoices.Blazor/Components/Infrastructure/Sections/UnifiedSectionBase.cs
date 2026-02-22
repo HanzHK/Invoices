9d79061033f74f1a7b0343d8eb94660db3631154
HanzHKR <kubicek.jan@hotmail.com>
2026-02-22 20:27:13 +0100

    - add sectionbase: supports setting number of columns

---
+++ Invoices.Blazor/Components/Infrastructure/Sections/UnifiedSectionBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Invoices.Blazor.Components.Infrastructure.Localization;
+
+namespace Invoices.Blazor.Components.Infrastructure.Sections;
+
+public abstract class UnifiedSectionBase : LocalizationComponentBase
+{
+    [CascadingParameter] public int Columns { get; set; } = 1;
+
+    protected int ColumnSize => 12 / Columns;
+}
