ff0f3aefc14b5c1636b2ff863d8c51ae555cb5d5
HanzHKR <kubicek.jan@hotmail.com>
2026-04-27 23:45:28 +0200

    - set up CreateInvoicePage

---
+++ Invoices.Blazor/Pages/Invoice/CreateInvoicePage.razor.cs
@@ -0,0 +1,27 @@
+﻿using Invoices.Blazor.Components.Infrastructure.Localization;
+using Invoices.Blazor.Services;
+using Invoices.Shared.Models.Invoice;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+
+namespace Invoices.Blazor.Pages.Invoice
+{
+   
+    public partial class CreateInvoicePage : LocalizationPageBase
+    {
+        private InvoicePostDto invoice = new();
+        [Inject] public InvoiceService InvoiceService { get; set; } = default!;
+
+        private async Task CreateInvoiceAsync(InvoicePostDto model)
+        {
+            var created = await InvoiceService.CreateAsync(model);
+            if (created != null)
+            {
+                Snackbar.Add(T("InvoiceAdded"), Severity.Success);
+                Nav.NavigateTo("/invoices/list");
+                return;
+            }
+        }
+    }
+}
